475b9541b9a09c62ac15bf9901335c9fea7a7599
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-12 10:54:42 -0300

    First commit on the proyect

---
+++ src/scope/global.js
@@ -0,0 +1,10 @@
+//Podemos establecer una variable a nivel global. 
+//Veamos un ejemplo: 
+var hello = "Hello";
+
+//Al establecerla de forma global nosotros podemos acceder a ella desde cualquier parte de nuestro codigo.
+//Por ejemplo, puedo llamarla de la siguiente manera
+
+console.log(hello) //Hello
+
+//Veamos que ocurre 
---
+++ src/scope/que_es.js
@@ -0,0 +1,3 @@
+//El scope se define como el alcance que va a tener tu variable dentro de tu codigo. 
+//O en otras palabras: El scope se va a encargar de definir a que bloque de codigo va a acceder una varible. 
+//Tenemos dos tipos de scope. El scope local y el scope global. 
