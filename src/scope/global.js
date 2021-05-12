af3e9e42fa40c42a13ae5e7bedf72d45ff1946b1
Rodrigo Juarez <juarezrodrigo59@gmail.com>
2021-05-12 11:09:48 -0300

    Modifications in var, let y const

--- src/scope/global.js
+++ src/scope/global.js
@@ -7,4 +7,41 @@ var hello = "Hello";
 
 console.log(hello) //Hello
 
-//Veamos que ocurre 
+//Veamos que ocurre cuando utilizamos let y const 
+
+var saludoVar = "Hello en la variable Var"
+let saludoLet = "Hello en la variable Let"
+const saludoConst = "Hello en la variable Const"
+
+const anotherFunction = () =>{
+    console.log(saludoVar)
+    console.log(saludoLet)
+    console.log(saludoConst)
+}
+
+anotherFunction()
+
+//Este codigo va a mostrar en consola:
+//  Hello en la variable Var
+//  Hello en la variable Let
+//  Hello en la variable Const
+
+//Esto ocurre por que las variables, tanto en var, let y const estan declara de forma global, de modo que todos pueden acceder a ella. 
+// --------- Nota importante ---------
+/*
+Reasignar una varible en el scope global es una mala practica. 
+Aunque con Var LO PODREMOS HACER. 
+En cambio con Let NO LO PODREMOS HACER. 
+Pero let si nos permite asignarle un nuevo valor a una variable. 
+Veamos un ejemplo:
+
+//Esto No !!
+let hlet = "hlet"
+let hlet = "hlet ..."
+
+//Esto SI !!
+let hlet = "hlet"
+hlet = "hlet_update"
+
+Con la variable CONST no podremos ni DECLARAR NUEVAMENTE LA VARIABLE, NI TAMPOCO ASIGNARLE UN VALOR POR SEGUNDA VEZ.
+*/
